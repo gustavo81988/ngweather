0a393d85c9d32684223342776eecc56c79f0795a
Gustavo Ramírez <gustavo81988@gmail.com>
2017-05-30 12:06:27 -0430

    Adding a Service

--- src/app/weather/weather-list.component.ts
+++ src/app/weather/weather-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {WeatherItem} from './weather-item';
 import {WEATHER_ITEMS} from './weather.data';
+import {WeatherService} from './weather.service';
 
 @Component({
   selector: 'weather-list',
@@ -9,12 +10,15 @@ import {WEATHER_ITEMS} from './weather.data';
           <weather-item *ngFor="let weatherItem of weatherItems" [item]="weatherItem"></weather-item>
       </section>
   `,
-  styles: []
+  styles: [],
+  providers:[WeatherService]
 })
 export class WeatherListComponent implements OnInit{
     weatherItems: WeatherItem[];
     
+    constructor(private weatherService: WeatherService){}
+
     ngOnInit():any{
-        this.weatherItems = WEATHER_ITEMS;
+        this.weatherItems = this.weatherService.getWeatherItems();
     }
 }
